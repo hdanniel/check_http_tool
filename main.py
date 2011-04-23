301dd96821dd7f59689dd29fe8005211ca4ec577
Hector Paz <hdanniel@gmail.com>
2011-04-22 21:01:27 -0500

    handling redirects

--- main.py
+++ main.py
@@ -14,15 +14,24 @@ import BaseHTTPServer
 #          self.assert_(get_valid_url('test')) 
 #          self.assert_(get_valid_url(1)) 
 
+#redirect_list=[]
+
 class RedirectHandler(urllib2.HTTPRedirectHandler):
+	def __init__(self):
+        	self.redirect_list = [] 
+
         def http_error_301(self, req, fp, code, msg, headers):  
-                result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)              
+                result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
                 result.redirect = code
+		#self.redirect_list.append((code,result.geturl()))
+		self.redirect_list.append((code,headers['Location']))
 		return result 
 
         def http_error_302(self, req, fp, code, msg, headers):
                 result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)              
                 result.redirect = code                                
+		#self.redirect_list.append((code,result.geturl()))
+		self.redirect_list.append((code,headers['Location']))
 		return result 
 
 def get_valid_url(url):
@@ -39,29 +48,29 @@ def get_response(url):
     request = urllib2.Request(url)
     request.add_header('Accept-encoding', 'gzip,deflate') 
     request.add_header('User-Agent', 'Mozilla/5.0 (X11; U; Linux x86_64; es-AR; rv:1.9.2.3) Gecko/20100423 Ubuntu/10.04 (lucid) Firefox/3.6.3')
-    opener = urllib2.build_opener(RedirectHandler)
+    rh = RedirectHandler()
+    redirect = []
+    opener = urllib2.build_opener(rh)
     urllib2.install_opener(opener) 
     try:
         response = urllib2.urlopen(request)
         if response: 
            error = False
+           redirect = rh.redirect_list
     except urllib2.HTTPError,e:
         response = False
         error = e.code
     except urllib2.URLError,e:
         response = False
         error = False
-    return response, error
+    return response, error, redirect
       
 def test_url(url):
-    response,error = get_response(url)
-    redirect = False
-    if hasattr(response, 'redirect'):
-       redirect = response.redirect 
+    response,error,redirect = get_response(url)
     if (response):
-       return response.info().get('Content-Encoding'), response.code, response.geturl(),redirect
+       return response.info().get('Content-Encoding'), response.code, redirect
     else:
-       return False,error,url,False
+       return False,error,False
 
 def main():
    p = OptionParser(description="HTTP Compression Test", prog="front.py", version="0.1", usage="%prog [url]")
@@ -69,13 +78,22 @@ def main():
    if len(arguments) > 0:
       for url in arguments:
           url = get_valid_url(url)
-          compression, code, real_url,redirect = test_url(url)
-          url_redirect = "" 
-          if redirect: 
-	     code = redirect
-	     url_redirect = real_url
+          compression, code, redirect = test_url(url)
+          #if redirect: 
+	  #   code = redirect
+	  #   url_redirect = real_url
           message_short, message_long = BaseHTTPServer.BaseHTTPRequestHandler.responses[code]
-          print str(code) + " : " + message_short + " " + url_redirect
+	  print 'URL: %s (%s - %s)' % (url, str(code), message_short)
+          #print 'Respuesta: %s - %s' % (str(code), message_short)
+	  if redirect:
+	     message_redirect = url 
+             for redirect_code,redirect_url in redirect[::-1]:
+	         message_redirect += " -> " 
+	         message_redirect += redirect_url 
+	         message_redirect += " (" 
+	         message_redirect += str(redirect_code)
+	         message_redirect += ") " 
+	     print message_redirect
           if (code in [200,301,302]):
              if compression :
                 print '%s soporta compresión HTTP' % url
